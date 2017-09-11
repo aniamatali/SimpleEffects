63fa5393356d444ee20c96c42e73d68777b77ff1
Alvin Niamatali and Hansen Hansen <hansen@>
2017-09-11 13:57:59 -0700

    jquery effects walrus page complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#initially-showing").fadeToggle();
-    $("#initially-hidden").slideToggle();
+    $(".initially-showing").toggle();
+    $(".initially-hidden").toggle();
+  });
+});
+
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $(".initially-showing2").fadeToggle();
   });
 });
